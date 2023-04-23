19df2aee0c352b0bb7a50a99b0c4cd08f0f7c0ae
Krisna Pranav <krisna.pranav@gmail.com>
2023-04-23 14:40:21 +0530

    contracts: AbsPod

--- contracts/AbsPoD.sol
+++ contracts/AbsPoD.sol
@@ -1,14 +1,21 @@
 pragma solidity ^0.4.18;
 
-contract AbsPod {
-    function resetWeiBalance(address _user) public returns(bool);
+contract AbsPoD {
 
-    function getBalanceOfToken(address _user) public constant returns(uint256);
+  function resetWeiBalance(address _user) public returns(bool);
 
-    function getCapOfToken() public constant returns(uint256);
+  function getBalanceOfToken(address _user) public constant returns(uint256);
 
-    function getStartTime() public constant returns(uint256);
+  function getCapOfToken() public constant returns(uint256);
 
-    function getEndtime() public constant returns(uint256);
+  function isPoDStarted() public constant returns(bool);
+
+  function isPoDEnded() public constant returns(bool);
+
+  function getTokenPrice() public constant returns(uint256);
+
+  function getStartTime() public constant returns (uint256);
+
+  function getEndtime() public constant returns(uint256);
 
 }
